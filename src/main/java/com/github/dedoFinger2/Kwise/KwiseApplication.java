7a0afd8f185b1253cd1ab5621a6fcfb1a7ab8872
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-17 08:42:41 -0300

    chore: init

---
+++ src/main/java/com/github/dedoFinger2/Kwise/KwiseApplication.java
@@ -0,0 +1,13 @@
+package com.github.dedoFinger2.Kwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dedoFinger2/Kwise/KwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dedoFinger2.Kwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
